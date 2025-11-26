fdbc30e31c48d0aaf77c7dc73041e6612a30b517
judigot <judigot@gmail.com>
2025-11-27 00:03:50 +0800

    Update

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import express, { type Request, type Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import process from 'node:process';
-const router = require('./route/index.ts');
+import router from './route/index.ts';
 
 dotenv.config();
 
@@ -17,14 +17,12 @@ app.get('/api', (_req: Request, res: Response) => {
   res.send({ message: router() });
 });
 
-// Use routes from the routes folder
-// app.use('/api', router);
-
-app.listen(VITE_BACKEND_PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(
-    `${platform.charAt(0).toUpperCase() + platform.slice(1)} is running on ${String(process.env.VITE_BACKEND_HOST)}:${VITE_BACKEND_PORT}`,
-  );
-});
+if (process.env.VERCEL !== '1') {
+  app.listen(VITE_BACKEND_PORT, () => {
+    console.error(
+      `${platform.charAt(0).toUpperCase() + platform.slice(1)} is running on ${String(process.env.VITE_BACKEND_HOST)}:${VITE_BACKEND_PORT}`,
+    );
+  });
+}
 
 export default app;
