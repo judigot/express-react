93612cb3b0eb36e955e6630163191e21707f2ce7
judigot <judigot@gmail.com>
2025-11-26 09:01:51 +0800

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,7 @@
-import React, { useEffect } from 'react';
-
-interface IData {
-  message: string;
-}
-
 function App() {
-  const [data, setData] = React.useState<IData | undefined>(undefined);
-
-  useEffect(() => {
-    const apiUrl = import.meta.env.VITE_API_URL ?? 'api/hello';
-    const backendUrl = import.meta.env.VITE_BACKEND_URL;
-    const url = backendUrl ? `${String(backendUrl)}/${String(apiUrl)}` : `/${String(apiUrl)}`;
-
-    fetch(url, {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((response) => response.json())
-      .then((result: IData | undefined) => {
-        if (result) {
-          setData(result);
-        }
-      })
-      .catch((error: unknown) => {
-        throw error instanceof Error ? error : new Error(String(error));
-      });
-  }, []);
-
   return (
     <div style={{ zoom: '500%', textAlign: 'center' }}>
-      <pre>
-        <code>{JSON.stringify(data, null, 4)}</code>
-      </pre>
+      <h1>Hello World</h1>
     </div>
   );
 }
