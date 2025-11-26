bc19adba6b9a97acb3951878ae92bc87661f811b
judigot <judigot@gmail.com>
2025-11-27 00:12:52 +0800

    feat: use alias with js extension

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import express, { type Request, type Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import process from 'node:process';
-import router from './route/index.js';
+import router from '@/route/index.js';
 
 dotenv.config();
 
